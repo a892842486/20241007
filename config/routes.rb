087058f32d0377b2da7515470cd9380dcd6022b5
Bamu <a892842486@github.com>
2024-10-10 12:27:47 +0800

    implement carts index function

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ root 'products#index'
 namespace :admin do
   resources :products
 end
+
 resources :products do
   member do
     post :add_to_cart
   end
 end
+
+resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
