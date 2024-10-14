d5541a2659c966e9989145ce4d35a93fb5264f52
Bamu <a892842486@github.com>
2024-10-14 10:24:27 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 root 'products#index'
 
-resources :orders
+namespace :admin do
+  resources :products
+  resources :orders do
+    member do
+      post :cancel
+      post :ship
+      post :shipped
+      post :return
+    end
+  end
+end
 
 resources :products do
   member do
